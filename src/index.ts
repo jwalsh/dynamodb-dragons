a5701ae2bcf925f55ade7f865d90106fb5a93df8
Jason Walsh <j@wal.sh>
2021-04-04 16:00:15 -0400

    listTables

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb"
 
 (async () => {
-    const client = new DynamoDB({
-        apiVersion: "2012-08-10",
-        endpoint: "http://localhost:8000",
-        region: "us-east-1"
-    });
-    
-    console.log(client.config.endpoint());  
+    console.log('DynamoDB')
+})();
 
-})()
+const client = new DynamoDB({
+    apiVersion: "2012-08-10",
+    endpoint: "http://localhost:8000",
+    region: "us-east-1"
+});
+
+console.log(client.config.endpoint());
+
+client.listTables({}, (err, data) => {
+    console.log(data);
+})
